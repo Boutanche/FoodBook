ebb4ee2b1a246cded5ca7ead66bc523043352e8a
Benoit BERTRAND <benoit.bertrand@live.fr>
2021-05-27 12:30:01 +0200

    [Client] Mise à jour du projet

--- FoodBook/Client/BLLC/Services/RestaurantService.cs
+++ FoodBook/Client/BLLC/Services/RestaurantService.cs
@@ -20,7 +20,13 @@ namespace BLLC.Services
             _httpClient = new HttpClient();
             _httpClient.BaseAddress = new Uri("https://localhost:5001/api/v1.0/");
         }
-
+        //TODO : Faire des régions : ce qui concerne les plats, les ingrédients, etc...
+        
+        /// <summary>
+        /// Créer un plat
+        /// </summary>
+        /// <param name="newDish"></param>
+        /// <returns></returns>
         public async Task<Dish> CreateDish(Dish newDish)
         {
             var response = await _httpClient.PostAsync("dish",
@@ -41,7 +47,11 @@ namespace BLLC.Services
                 return null;
             };
         }
-
+        /// <summary>
+        /// Créer un ingrédient
+        /// </summary>
+        /// <param name="ingredient"></param>
+        /// <returns></returns>
         public async Task<Ingredients> CreateIngredients(Ingredients ingredient)
         {
             var response = await _httpClient.PostAsync("ingredients",
@@ -63,7 +73,19 @@ namespace BLLC.Services
                 return null;
             };
         }
-
+        /// <summary>
+        /// Créer un élément de liaison entre un plat et un ingrédient
+        /// </summary>
+        /// <param name="listOfIngredient"></param>
+        /// <returns></returns>
+        public Task<ListOfIngredient> CreateListOfIngredient(ListOfIngredient listOfIngredient)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Récupérer la liste de tous les ingrédients.
+        /// </summary>
+        /// <returns></returns>
         public async Task<List<Ingredients>> GetAllIngredients()
         {
             //var reponse = await _httpClient.GetAsync($"ingredients?page={pageRequest.Page}&pageSize={pageRequest.PageSize}");
@@ -87,7 +109,15 @@ namespace BLLC.Services
                 return null;
             }
         }
-  
 
+        /// <summary>
+        /// Récupérer un plat par son nom
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Task<Dish> GetDishByName(string name)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
