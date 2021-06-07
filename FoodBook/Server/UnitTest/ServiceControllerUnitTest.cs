444029732cee6ed756b9a9cf7dcf16fb973c668e
Benoit BERTRAND <benoit.bertrand@live.fr>
2021-06-07 15:45:01 +0200

    [Server] Add To Unit Test : Booking, Client, Order and Service.

---
+++ FoodBook/Server/UnitTest/ServiceControllerUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using API.Controllers;
+using BLL.Services;
+using BO.DTO.Requests;
+using BO.Entity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using UnitTest.Services;
+using Xunit;
+
+namespace UnitTest
+{
+    class ServiceControllerUnitTest
+    {
+    }
+}
