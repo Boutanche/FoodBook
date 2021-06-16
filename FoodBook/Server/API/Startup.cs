e1d123d00f48854d4f1b611b81d57aa806078797
Benoit BERTRAND <benoit.bertrand@live.fr>
2021-06-16 09:43:35 +0200

    [Server] Update data and trace

--- FoodBook/Server/API/Startup.cs
+++ FoodBook/Server/API/Startup.cs
@@ -26,6 +26,7 @@ namespace API
     {
         public Startup(IConfiguration configuration)
         {
+            Trace.WriteLine("...Lancement du Startup ...");
             Configuration = configuration;
         }
 
@@ -34,7 +35,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            Trace.WriteLine("...Lancement de ConfigureServices...");
+            Trace.WriteLine("...Lancement de Configure Services...");
             //Unique dans toute mon application (même instance dans toute l'aplication)
             //A chaque fois qu'on demande un Ilibrairi service => nouvelle instance
             //services.AddTransient<IRestaurantService, RestaurantService>();
@@ -45,6 +46,7 @@ namespace API
             //Service de Versionning
             services.AddApiVersioning(config =>
             {
+                Trace.WriteLine("Service : Versions d'API ...");
                 //Renvoie dans le header les versions d'API supportées.
                 config.ReportApiVersions = true;
             });
@@ -52,6 +54,7 @@ namespace API
 
             services.AddVersionedApiExplorer(options =>
             {
+                Trace.WriteLine("Service : Explorateur Versions d'API ");
                 options.SubstituteApiVersionInUrl = true;
             });
 
@@ -78,10 +81,12 @@ namespace API
                     };
                 };
                 config.ApiGroupNames = new[] { "1.0" };
+                Trace.WriteLine("Service : Mise à disposition de la documentation :  ");
             });
             //End OpenAPI Document
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
+                Trace.WriteLine("Service : Mise en place de Authentification  ");
                 options.TokenValidationParameters = new TokenValidationParameters()
                 {
                     ValidateIssuer = true,
@@ -97,7 +102,9 @@ namespace API
         
 
             services.AddControllers();
+            Trace.WriteLine("Service : Controllers ");
             services.AddBLL();
+            Trace.WriteLine("Service : Couche Bibliothèque Logique");
 
         }
 
@@ -106,18 +113,23 @@ namespace API
         {
             if (env.IsDevelopment())
             {
+                Trace.WriteLine("Seulement en environnement de développement : ");
+                Trace.WriteLine("Utilisation de la page d'exception pour les développeurs.");
                 app.UseDeveloperExceptionPage();
+
             }
             
             //DocFX
             app.UseDocFx(config =>
             {
+                Trace.WriteLine("Mise en place de la route pour configuration la documentation :  ");
                 config.rootPath = "/doc";
             });
 
             //Middleware de génération du OpenAPIjson
             app.UseOpenApi(config =>
             {
+                Trace.WriteLine("Mise en place de la route pour consulter la documentation :  ");
                 config.Path = "/api/doc/{documentName}/api.json";
             });
             //END
@@ -125,24 +137,30 @@ namespace API
             // Interface utilisateur en fonction du OpenApi.json
             app.UseSwaggerUi3(config =>
             {
+                Trace.WriteLine("Mise en place de l'interface pour la documentation :  ");
                 config.DocumentPath = "/api/doc/{documentName}/api.json";
                 config.Path = "/api/doc";
             });
             //END
 
             app.UseHttpsRedirection();
+            Trace.WriteLine("Mise en place de la route pour HTTPS :  ");
 
             app.UseRouting();
+            Trace.WriteLine("Mise en place des routes :  ");
 
             //CE : 403
             app.UseAuthorization();
+            Trace.WriteLine("Mise en place des authorisations :  ");
 
             //CE : 401
             app.UseAuthentication();
+            Trace.WriteLine("Mise en place des authentifications : ");
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                Trace.WriteLine("Mappage de API : ");
             });
 
             
